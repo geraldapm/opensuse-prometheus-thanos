ced9bdb0d08502459b4c99d485ab8359fd54e58b
Gerald <geraldiaditya1111@gmail.com>
2026-07-27 20:35:22 +0700

    fix(rustfs): full rustfs image path

--- deploy-rustfs.sh
+++ deploy-rustfs.sh
@@ -11,4 +11,4 @@ mkdir -p $WORKING_DIR/data $WORKING_DIR/logs
 podman run --replace --name rustfs -d -p 9000:9000 -p 9001:9001 \
     -e RUSTFS_ACCESS_KEY="gpmrustfs" \
     -e RUSTFS_SECRET_KEY="0255ec0a-72ee-448c-8823-2c652fc11a13" \
-    -v ${WORKING_DIR}/data:/data:Z,U -v ${WORKING_DIR}/logs:/logs:Z,U rustfs/rustfs:latest
+    -v ${WORKING_DIR}/data:/data:Z,U -v ${WORKING_DIR}/logs:/logs:Z,U docker.io/rustfs/rustfs:latest
